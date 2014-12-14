ccc4c79502f529b3cc6d074890a21eb8e8adb114
Alec Henninger <alechenninger@gmail.com>
2014-12-14 17:06:06 -0500

    Rearrange imports

--- lib/comet.dart
+++ lib/comet.dart
@@ -1,13 +1,12 @@
 library comet;
 
 import 'package:irc/client.dart';
-
+import 'package:uuid/uuid.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:shelf_route/shelf_route.dart';
 import 'package:shelf_static/shelf_static.dart';
 import 'package:shelf_web_socket/shelf_web_socket.dart';
-import 'package:uuid/uuid.dart';
 
 import 'dart:async';
 import 'dart:convert';
